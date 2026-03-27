662417dbc05abc97297415bd3b23b544c3b861d4
legione8512 <marius.robert@gmail.com>
2026-03-27 23:33:16 +0200

    Initial project structure

---
+++ backend/src/main/java/com/dayalog/DayalogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dayalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayalogBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dayalog/DayalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
