fe300d1951a378bf3c55a0dd95de89b47ac048bd
legione8512 <marius.robert@gmail.com>
2026-03-27 23:47:55 +0200

    Re inittialized project

--- backend/src/main/java/ro/daya/dayalog/DayalogBackendApplication.java
+++ backend/src/main/java/ro/daya/dayalog/DayalogBackendApplication.java
@@ -1,4 +1,4 @@
-package com.dayalog;
+package ro.daya.dayalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/ro/daya/dayalog/DayalogBackendApplicationTests.java
+++ backend/src/test/java/ro/daya/dayalog/DayalogBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dayalog;
+package ro.daya.dayalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
